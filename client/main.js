98743106458e98cf18c8f08f11cdc6f5b4451b0f
michael mason <michael@michaeljmason.com>
2017-04-04 12:23:37 -0700

    working with MiniMongo

--- client/main.js
+++ client/main.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+import {Players} from './../imports/api/players';
 
-
-const players = [{
-  _id: '1',
-  name: 'Boone',
-  score: 99
-}, {
-  _id: '2',
-  name: 'Ken',
-  score: 22
-}, {
-  _id: '3',
-  name: 'Ed',
-  score: 17
-}];
 
 const renderPlayers = function (playersList) {
   return playersList.map(function (player) {
@@ -23,16 +11,20 @@ const renderPlayers = function (playersList) {
   });
 };
 
+
 Meteor.startup(function() {
-  let title = 'Score Keep';
-  let name = 'Boone';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      <p>Hello {name}</p>
-      <p>Second paragraph</p>
-      {renderPlayers(players)}
-    </div>
-  );
-    ReactDOM.render(jsx, document.getElementById('app'));
+  Tracker.autorun(function(){
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let name = 'Boone';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}</p>
+        <p>Second paragraph</p>
+        {renderPlayers(players)}
+      </div>
+    );
+      ReactDOM.render(jsx, document.getElementById('app'));
+  });
 });
