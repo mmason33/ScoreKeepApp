8511e90fc1ae0de1ca319470bf814c2e2cd5ac63
michael mason <michael@michaeljmason.com>
2017-04-04 15:03:50 -0700

    added Players collection, added logic to insert/remove players, inc/dec score by one via update method

--- client/main.js
+++ client/main.js
@@ -5,24 +5,43 @@ import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
 
 
-const renderPlayers = function (playersList) {
-  return playersList.map(function (player) {
-    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
+const renderPlayers = (playersList) => {
+  return playersList.map((player) => {
+    return (
+      <p key={player._id}>
+        {player.name} has {player.score} point(s)
+        <button onClick={() => Players.update(player._id, {$inc: {score: -1}})}>-1</button>
+        <button onClick={() => Players.update(player._id, {$inc: {score: 1}})}>+1</button>
+        <button onClick={() => Players.remove(player._id)}>X</button>
+      </p>
+    );
   });
 };
 
+const handleSubmit = (e) => {
+  let playerName = e.target.playerName.value;
+  e.preventDefault();
+  if (playerName) {
+    e.target.playerName.value = '';
+    Players.insert({
+      name: playerName,
+      score: 0
+    });
+  };
+};
 
-Meteor.startup(function() {
-  Tracker.autorun(function(){
+Meteor.startup(() => {
+  Tracker.autorun(() => {
     let players = Players.find().fetch();
     let title = 'Score Keep';
-    let name = 'Boone';
     let jsx = (
       <div>
         <h1>{title}</h1>
-        <p>Hello {name}</p>
-        <p>Second paragraph</p>
         {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player Name"/>
+          <button>Add Player</button>
+        </form>
       </div>
     );
       ReactDOM.render(jsx, document.getElementById('app'));
--- server/main.js
+++ server/main.js
@@ -1,11 +1,6 @@
 import {Players} from './../imports/api/players';
 import {Meteor} from 'meteor/meteor';
 
-Meteor.startup(function() {
-  Players.insert({
-    name: 'Ken',
-    score: '17'
-  });
+Meteor.startup(() => {
 
-  console.log(Players.find().fetch() );
 });
